e5120f9ee21cf9bc917863caee4684288cfba51d
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2025-01-24 19:56:01 +0300

    Bingo card created works

---
+++ src/BingoGame.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BingoGame = () => {
+  return (
+    <View>
+      <Text>BingoGame</Text>
+    </View>
+  )
+}
+
+export default BingoGame
